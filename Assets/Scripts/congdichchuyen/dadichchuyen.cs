97e4f0889d33e943c6d11a71d7b846ca148df3f0
trideptraivcl <tridmps38521@gmail.com>
2024-11-29 11:00:04 +0700

    skill mới au nhây con boss

---
+++ Assets/Scripts/congdichchuyen/dadichchuyen.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class dadichchuyen : MonoBehaviour
+{
+    [SerializeField] Transform diemDichChuyenDen;
+    public Transform GetDiemDichChuyenDen()
+    {
+        return diemDichChuyenDen;
+    }
+}

eab1d09ffef367c80c6bcb6d5097ecb3bc2a805e
HThuan <thuanvhps38410@gmail.com>
2024-12-03 00:46:03 +0700

    ruong

---
+++ Assets/Scripts/Ruong/ChestInteraction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ChestInteraction : MonoBehaviour
+{
+    public Sprite openChest; // Sprite khi rương mở
+    private SpriteRenderer spriteRenderer;
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && Input.GetKeyDown(KeyCode.F))
+        {
+            OpenChest();
+        }
+    }
+
+    private void OpenChest()
+    {
+        if (!isOpen) // Nếu rương chưa mở
+        {
+            spriteRenderer.sprite = openChest; // Đổi sprite sang trạng thái mở
+            isOpen = true; // Đánh dấu rương đã mở
+            Debug.Log("Chest opened!");
+            // Thêm logic khác (nếu cần) như trao phần thưởng
+        }
+        else
+        {
+            Debug.Log("Chest is already open.");
+        }
+    }
+}

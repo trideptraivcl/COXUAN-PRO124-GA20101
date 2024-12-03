1f0d10f58db43fdc4cde0ff67e17a695dd12a1f1
HThuan <thuanvhps38410@gmail.com>
2024-12-04 03:47:23 +0700

    22

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class PauseMenu : MonoBehaviour
     {
         PausePanel.SetActive(true);
         Time.timeScale = 0;
+    }
+    public void Exit()
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1;
     }
     public void Continue()
     {

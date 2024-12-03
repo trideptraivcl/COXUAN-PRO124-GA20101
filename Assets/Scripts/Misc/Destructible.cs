6ae0299698a51020a7352bcc229a7292abca0c66
HThuan <thuanvhps38410@gmail.com>
2024-12-03 18:31:24 +0700

    coin

--- Assets/Scripts/Misc/Destructible.cs
+++ Assets/Scripts/Misc/Destructible.cs
@@ -8,6 +8,7 @@ public class Destructible : MonoBehaviour
     {
         if (other.gameObject.GetComponent<DamegeSource>() || other.gameObject.GetComponent<Projectile>())
         {
+            GetComponent<PickUpSpawner>().DropItems();
             Instantiate(destroyVFX, transform.position, 
                 Quaternion.identity);
             Destroy(gameObject);

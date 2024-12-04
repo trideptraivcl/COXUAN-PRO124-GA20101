e2518f95f30a31e1ed6776335add22deb046bb78
trideptraivcl <tridmps38521@gmail.com>
2024-12-04 12:42:20 +0700

    ư

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public float delaySecond = 4f; // Thời gian chờ trước khi chuyển scene
+    public string nameScene = "map2"; // Tên scene sẽ được tải
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.SetActive(false); // Vô hiệu hóa đối tượng Player
+            ModeSelect(); // Gọi hàm chuyển scene
+        }
+    }
+
+    public void ModeSelect()
+    {
+        StartCoroutine(LoadAfterDelay()); // Bắt đầu coroutine
+    }
+
+    IEnumerator LoadAfterDelay()
+    {
+        yield return new WaitForSeconds(delaySecond); // Chờ trong khoảng thời gian chỉ định
+        SceneManager.LoadScene(nameScene); // Chuyển đến scene mới
+        Destroy(gameObject);
+
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Sửa lại 'SceneManagerment' thành 'SceneManagement'
+using System.Collections;
+using System.Collections.Generic;
+
+public class Menu : MonoBehaviour
+{
+    // Phương thức PlayGame sẽ được gọi khi người chơi nhấn nút Play
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1); // Tải scene có chỉ số 1 (thường là scene thứ hai trong Build Settings)
+    }
+}
